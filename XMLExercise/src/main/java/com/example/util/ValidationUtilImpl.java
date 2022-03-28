e3bd30ccaa312dedd48ebd5a33757ff5d5de9a00
Danail <d.zahariev@icloud.com>
2022-03-28 21:03:17 +0300

    update

--- XMLExercise/src/main/java/com/example/util/ValidationUtilImpl.java
+++ XMLExercise/src/main/java/com/example/util/ValidationUtilImpl.java
@@ -10,7 +10,7 @@ public class ValidationUtilImpl implements ValidationUtil {
 
     private final Validator validator;
 
-    public ValidationUtilImpl(Validator validator) {
+    public ValidationUtilImpl() {
         this.validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
